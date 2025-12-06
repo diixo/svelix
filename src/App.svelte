c82780354c199876060a06917ec108484ea3fce6
diixo <anatolii.podoba@gmail.com>
2025-12-06 06:16:28 +0200

    Added (initial commit) about <-> main

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,40 @@
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
+
+
 <script>
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import About   from './lib/About.svelte'
+  let page = 'home'; // 'home' или 'about'
 </script>
+
+<nav>
+  <button on:click={() => page = 'home'}>Main</button>
+  <button on:click={() => page = 'about'}>About-section</button>
+</nav>
+
+{#if page === 'home'}
+  <Counter />
+{:else if page === 'about'}
+  <About />
+{/if}
 
 <main>
   <div>
@@ -28,20 +60,3 @@
   </p>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
