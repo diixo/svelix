92a398aa3db438c2d0edb5a15a82f49195079a45
diixo <anatolii.podoba@gmail.com>
2025-12-06 06:26:58 +0200

    App.svelte: updated

--- src/App.svelte
+++ src/App.svelte
@@ -30,13 +30,14 @@
   <button on:click={() => page = 'about'}>About-section</button>
 </nav>
 
+
+<main>
 {#if page === 'home'}
   <Counter />
 {:else if page === 'about'}
   <About />
 {/if}
 
-<main>
   <div>
     <a href="https://vite.dev" target="_blank" rel="noreferrer">
       <img src={viteLogo} class="logo" alt="Vite Logo" />
@@ -46,10 +47,6 @@
     </a>
   </div>
   <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
 
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
